f5a7957afbfe87a1fc8f110ad7f495c202418225
Sandro Gakharia <sandrogakharia@gmail.com>
2025-07-21 16:55:47 +0400

    Initial Commit

---
+++ TimesTables/TimesTablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimesTablesApp.swift
+//  TimesTables
+//
+//  Created by Sandro Gakharia on 21.07.25.
+//
+
+import SwiftUI
+
+@main
+struct TimesTablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TimesTablesTests/TimesTablesTests.swift
@@ -0,0 +1,17 @@
+//
+//  TimesTablesTests.swift
+//  TimesTablesTests
+//
+//  Created by Sandro Gakharia on 21.07.25.
+//
+
+import Testing
+@testable import TimesTables
+
+struct TimesTablesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
